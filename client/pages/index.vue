f2d5e238dba06b2aac156cecb90e1599f7e23227
Akbar Joudi <akbar.joody@gmail.com>
2020-06-28 11:59:53 +0430

    add nuxt

---
+++ client/pages/index.vue
@@ -0,0 +1,69 @@
+<template>
+  <div>
+    <b-container>
+      <b-jumbotron
+        header="BootstrapVue"
+        lead="Bootstrap v4 Components for Vue.js 2"
+        bg-variant="white"
+      >
+        <p>For more information visit website</p>
+        <b-button variant="primary" href="#">More Info</b-button>
+      </b-jumbotron>
+    </b-container>
+    <b-container>
+      <b-row style="min-height:27rem">
+        <b-col>
+          <h2>Heading</h2>
+
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et
+            dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip
+            ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu
+            fugiat nulla pariatur.
+          </p>
+
+          <p>
+            <a class="btn btn-default" href="http://www.yiiframework.com/doc/">Yii Documentation »</a>
+          </p>
+        </b-col>
+        <b-col>
+          <h2>Heading</h2>
+
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et
+            dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip
+            ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu
+            fugiat nulla pariatur.
+          </p>
+          <p>
+            <a class="btn btn-default" href="http://www.yiiframework.com/doc/">Yii Documentation »</a>
+          </p>
+        </b-col>
+        <b-col>
+          <h2>Heading</h2>
+
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et
+            dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip
+            ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu
+            fugiat nulla pariatur.
+          </p>
+
+          <p>
+            <a class="btn btn-default" href="http://www.yiiframework.com/doc/">Yii Documentation »</a>
+          </p>
+        </b-col>
+      </b-row>
+    </b-container>
+  </div>
+</template>
+
+<script>
+export default {
+  head() {
+    return {
+      title: "yii2 for yii web application"
+    };
+  },
+};
+</script>
--- controllers/SiteController.php
+++ controllers/SiteController.php
@@ -61,7 +61,7 @@ class SiteController extends Controller
      */
     public function actionIndex()
     {
-        return $this->render('index');
+        return file_get_contents('_nuxt/index.html');
     }
 
     /**
